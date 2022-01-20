983a5aa12b766802c730e6d249e5bcfa40c38958
roboromeo1 <adhityaram417@gmail.com>
2022-01-21 06:04:39 +1100

    config:initial git commit

---
+++ src/domain/access_token/access_token_test.go
@@ -0,0 +1,19 @@
+package access_token
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestAccessTokenConstants(t *testing.T) {
+	assert.EqualValues(t, 24, expirationTime, "expiration time should be 24 hours")
+}
+
+func TestAccessTokenIsExpired(t *testing.T) {
+	at := AccessToken{}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
+
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	assert.False(t, at.IsExpired(), "access token expiring three hours from now should NOT be expired")
+}
